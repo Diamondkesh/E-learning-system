7411eadac8dc9003c6b77ddd678f52e37fba9a2b
Hemkesh <hemkesh_molla@outlook.com>
2018-01-26 22:55:08 +0400

    Form convertor API building

--- modules/custom/site_api/js/form-convertor.js
+++ modules/custom/site_api/js/form-convertor.js
@@ -2,8 +2,11 @@
 	$(function() {
 		$('.node-slide-form').each(function() {
 			/* creation */
-			var slide = $('<div class="slide"></div>'),
-			widthOverlayTop = $('<div class="width-overlay top">Mousewheel</div>'),
+			var self = $(this),
+			widthField = $('#edit-field-slide-width-0-value', self),
+			heightField = $('#edit-field-slide-height-0-value', self),
+			slide = $('<div class="slide"></div>'),
+			widthOverlayTop = $('<div class="width-overlay top"></div>'),
 			widthOverlayBottom = $('<div class="width-overlay bottom"></div>'),
 			heightOverlayLeft = $('<div class="height-overlay left"></div>'),
 			heightOverlayRight = $('<div class="height-overlay right"></div>');
@@ -16,11 +19,13 @@
 
 					var slideWidth = slide.width();
 					switch (e.deltaY) {
-						case -1: //wheeling down
+						case 1: //wheeling down
 							slide.css('width', (slideWidth + 1) + 'px');
+							widthField.val(slideWidth + 1);
 							break;
-						case 1: //wheeling up
+						case -1: //wheeling up
 							slide.css('width', (slideWidth - 1) + 'px');
+							widthField.val(slideWidth - 1);
 							break;
 					}
 				});
@@ -31,18 +36,34 @@
 
 					var slideHeight = slide.height();
 					switch (e.deltaY) {
-						case -1: //wheeling down
+						case 1: //wheeling down
 							slide.css('height', (slideHeight + 1) + 'px');
+							heightField.val(slideHeight + 1);
 							break;
-						case 1: //wheeling up
+						case -1: //wheeling up
 							slide.css('height', (slideHeight - 1) + 'px');
+							heightField.val(slideHeight - 1);
 							break;
 					}
 				});
 
+			var bodySpace = $('<div class="body-space"></div>');
+
+			slide.append(bodySpace);
+
+			CKEDITOR.on('instanceReady', function(e) {
+				bodySpace.click(function() {
+					e.editor.focus();
+				});
+
+				e.editor.on('change', function() {
+					bodySpace.html(this.getData());
+				});
+			});
+
 			/* placement */
-			$(this).before(slide)
-				.css('display', 'none');
+			self.before(slide)
+				/*.css('display', 'none')*/;
 		});
 	});
 })(jQuery);
